825d9468e65ca5314054a540fe42f9d0433e9ec8
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2020-11-05 09:09:51 +0200

    Add MainMenu

--- Wakanda/Wakanda/ContentView.swift
+++ Wakanda/Wakanda/ContentView.swift
@@ -8,6 +8,8 @@
 
 import SwiftUI
 
+let mainBgColor: some View = Color.green.colorMultiply(Color.blue)
+
 struct ContentView: View {
     
     private var labels = ["MTN Mobile Money", "MTN Airtime & Internet", "Airtel Money", "Rwanda Useful Codes"]
@@ -17,98 +19,95 @@ struct ContentView: View {
     @State var column = 2
     var body: some View {
         GeometryReader { geo in
-            VStack(spacing: 0) {
-                HStack {
-                    Spacer()
-                    Text("Wakanda")
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.black)
-                        .shadow(radius: 4)
-                        
-                        .overlay(
-                            Text("Wakanda")
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                                .shadow(radius: 10)
-                    )
-                    Spacer()
-                    
-                }.overlay(
-                    Image(systemName: "ellipsis")
-                        .rotationEffect(.degrees(90)),
-                    alignment: .trailing)
-                    .padding()
-                    .foregroundColor(.white)
-                
-                VStack(spacing: -20) {
-                    Text("Pick a service")
-                        .font(.callout)
-                        .fontWeight(.regular)
-                        .foregroundColor(.white)
-                        .padding(.bottom, 40)
-                    VStack {
-                        
-                        GridStack(rows: 2, columns: 2) { row, column in
-                            VStack  {
-                                Image(self.images[self.index(at: row, and: column)])
-                                    .resizable()
-                                    .frame(width: 50, height: 50)
-                                    .padding(5)
-                                Text(self.labels[self.index(at: row, and: column)])
-                                    .font(.system(size: 14, weight: .regular))
-                                    .lineLimit(2)
-                                    .frame(maxWidth: .infinity)
-                                
-                            }
-                            .padding(.horizontal, 8)
-                            .frame(width: geo.size.width/2.2,
-                                   height: geo.size.width/2.2)
-                                .background(Color(red: 0.964, green: 0.97, blue: 0.976))
-                                .cornerRadius(10)
-                        }
-                        .padding()
-                        Spacer()
-                    }
-                    .frame(maxWidth: .infinity)
-                    .background(Color.black)
-                    .padding(.bottom, 20)
-                    .cornerRadius(30)
-                    
+            NavigationView {
+                VStack(spacing: 0) {
                     HStack {
-                        Image(systemName: "house.fill")
-                            .foregroundColor(.green)
                         Spacer()
-                        Image("hexa")
-                            .resizable()
-                            .frame(width: 60, height: 38, alignment: .bottom)
-                        Spacer()
-                    }
-                    .padding(6)
-                    .padding(.horizontal)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.white.shadow(color: Color.black, radius: 2).edgesIgnoringSafeArea(.bottom))
-                        
-                    .overlay(
-                        
-                        Image(systemName: "suit.heart.fill")
-                            .foregroundColor(Color.green)
-                            .frame(width: 50,height: 50)
-                            .background(Color.white)
-                            .clipShape(Circle())
-                            .padding(4)
-                            .background(Color.black)
-                            .clipShape(Circle())
+                        Text("Wakanda")
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(.black)
+                            .shadow(radius: 4)
                             
-                            .offset(x: -25, y: -25)
-                        , alignment: .topTrailing
-                    )
+                            .overlay(
+                                Text("Wakanda")
+                                    .font(.largeTitle)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                    .shadow(radius: 10)
+                        )
+                        Spacer()
+                        
+                    }.overlay(
+                        NavigationLink(destination: MenuView()) {
+                            Image(systemName: "ellipsis")
+                                .rotationEffect(.degrees(90))
+                        },
+                        alignment: .trailing)
+                        .padding()
+                        .foregroundColor(.white)
+                    
+                    VStack(spacing: -20) {
+                        Text("Pick a service")
+                            .font(.callout)
+                            .fontWeight(.regular)
+                            .foregroundColor(.white)
+                            .padding(.bottom, 40)
+                        VStack {
+                            
+                            GridStack(rows: 2, columns: 2) { row, column in
+                                ExtractedView(image: self.images[self.index(at: row, and: column)],
+                                              label: self.labels[self.index(at: row, and: column)])
+                                    
+                                    .frame(width: geo.size.width/2.2,
+                                           height: geo.size.width/2.2)
+                                    .background(Color(red: 0.964, green: 0.97, blue: 0.976))
+                                    .cornerRadius(10)
+                            }
+                            .padding()
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity)
+                        .background(Color.black)
+                        .padding(.bottom, 20)
+                        .cornerRadius(30)
+                        
+                        HStack {
+                            Image(systemName: "house.fill")
+                                .foregroundColor(mainFgColor)
+                            Spacer()
+                            Image("hexa")
+                                .resizable()
+                                .frame(width: 60, height: 38, alignment: .bottom)
+                            Spacer()
+                        }
+                        .padding(6)
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.white.shadow(color: Color.black, radius: 2).edgesIgnoringSafeArea(.bottom))
+                            
+                        .overlay(
+                            
+                            Image(systemName: "suit.heart.fill")
+                                .foregroundColor(mainFgColor)
+                                .frame(width: 50,height: 50)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                                .padding(4)
+                                .background(Color.black)
+                                .clipShape(Circle())
+                                
+                                .offset(x: -25, y: -25)
+                            , alignment: .topTrailing
+                        )
+                    }
+                    
                 }
-                
+                .frame(maxWidth: .infinity)
+                .background(mainBgColor.edgesIgnoringSafeArea(.all))
+            .navigationBarTitle("")
+            .navigationBarHidden(true)
             }
-            .frame(maxWidth: .infinity)
-            .background(Color.green.colorMultiply(Color.blue).edgesIgnoringSafeArea(.all))
         }
     }
     
@@ -145,5 +144,23 @@ struct GridStack<Content: View>: View {
         self.rows = rows
         self.columns = columns
         self.content = content
+    }
+}
+
+struct ExtractedView: View {
+    let image: String
+    let label: String
+    var body: some View {
+        VStack  {
+            Image(image)
+                .resizable()
+                .frame(width: 50, height: 50)
+                .padding(5)
+            Text(label)
+                .font(.system(size: 14, weight: .regular))
+                .lineLimit(2)
+                .frame(maxWidth: .infinity)
+        }
+        .padding(.horizontal, 8)
     }
 }
--- Wakanda/Wakanda/MenuView.swift
+++ Wakanda/Wakanda/MenuView.swift
@@ -8,31 +8,86 @@
 
 import SwiftUI
 
+let mainFgColor = Color(red: 0.011, green: 0.37, blue: 0.351)
+
 struct MenuView: View {
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        VStack {
-            VStack {
-                Text("Wakanda")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    
-                .overlay(
-                    Text("Wakanda")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                        .foregroundColor(.orange)
-                    .shadow(radius: 2)
-                )
-            }
-        }
         
-        .frame(width: 200, height: 200)
-        .background(Color.orange)
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "arrow.left")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 20, height: 20)
+                    .onTapGesture {
+                        withAnimation {                            self.presentationMode.wrappedValue.dismiss()
+                        }
+                }
+                Spacer().frame(width: 50)
+                Text("Main Menu")
+                    .font(.system(size: 20))
+                    .bold()
+            }
+            .foregroundColor(.white)
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .frame(height: UIScreen.main.bounds.size.height/4)
+            .background(mainBgColor.edgesIgnoringSafeArea(.top))
+            
+            
+            ScrollView {
+                MenuRow(image: "plus.circle.fill", title: "Save your accounts", subTitle: "Save your meter numbers etc")
+                MenuRow(image: "square.and.arrow.up.fill", title: "Share Wakanda", subTitle: "Share app with friends")
+                MenuRow(image: "person.3.fill", title: "Change Language", subTitle: "")
+                MenuRow(image: "exclamationmark.circle.fill", title: "About Wakanda", subTitle: "")
+                MenuRow(image: "bubble.left.and.bubble.right.fill", title: "FAQ", subTitle: "")
+                MenuRow(image: "envelope.fill", title: "Email Support", subTitle: "")
+                MenuRow(image: "lock.shield.fill", title: "Logout", subTitle: "")
+                
+                Text("Version: 3.0.0 Build: 30")
+                    .foregroundColor(Color(.lightGray))
+                .font(.system(size: 14, weight: .semibold))
+                
+            }
+            .padding(.leading)
+            .padding(.trailing, 5)
+        }.transition(.slide)
+        .navigationBarTitle("")
+        .navigationBarHidden(true)
     }
 }
 
 struct MenuView_Previews: PreviewProvider {
     static var previews: some View {
         MenuView()
+    }
+}
+
+struct MenuRow: View {
+    let image: String
+    let title: String
+    let subTitle:String
+    var body: some View {
+        HStack(spacing: 30) {
+            Image(systemName: image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 22, height: 22)
+                .foregroundColor(mainFgColor)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(title)
+                    .font(.system(size: 14, weight: .regular))
+                
+                if !self.subTitle.isEmpty {
+                    Text(subTitle)
+                        .foregroundColor(.gray)
+                        .font(.system(size: 12, weight: .regular))
+                }
+            }
+        }
+        .padding(10)
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
